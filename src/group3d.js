092b1bcd4ed37b8600574bd480faa4e0a22795f5
Stellan Gil <lotstar87@gmail.com>
2018-03-08 10:15:40 +0900

    v0.1.3
    extrude type 추가

--- src/group3d.js
+++ src/group3d.js
@@ -1,12 +1,16 @@
 /*
  * Copyright © HatioLab Inc. All rights reserved.
  */
+var { Component3d } = scene;
 
 export default class Group3D extends THREE.Group {
-  constructor(model, canvasSize) {
+  constructor(model, canvasSize, visualizer) {
     super();
 
     this._model = model;
+    this._visualizer = visualizer;
+    // this.createObject(canvasSize);
+    // this.createChildrenObject(canvasSize);
     // this.createObject(canvasSize);
   }
 
@@ -25,9 +29,45 @@ export default class Group3D extends THREE.Group {
     })
   }
 
-  createObject(canvasSize) {}
+  createObject(canvasSize) {
+
+    var {
+      left = 0,
+      top = 0,
+      width = 0,
+      height = 0
+    } = this.model
+
+    let cx = (left + (width / 2)) - canvasSize.width / 2
+    let cy = (top + (height / 2)) - canvasSize.height / 2
+    // let cz = this.model.rx
+
+    this.position.x = cx;
+    this.position.z = cy;
+  }
+
+  createChildrenObject(canvasSize) {
+    var { components } = this._model
+
+    components.forEach(component => {
+      var clazz = Component3d.register(component.type)
+      if (!clazz) {
+        console.warn("Class not found : 3d class is not exist");
+        return;
+      }
+
+      var item = new clazz(component, canvasSize, this._visualizer)
+      if (item) {
+        item.name = component.id;
+        this.add(item)
+        // this.putObject(component.model.id, item);
+      }
+    })
+  }
 
   get model() {
     return this._model
   }
 }
+
+Component3d.register('group', Group3D);
