325967038d558e59831d3168994c5af4e53a0258
Stellan Gil <lotstar87@users.noreply.github.com>
2018-07-06 17:42:22 +0900

    compress asset
    fix hatiolab/things-point#98

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require('path')
 
-const webpack = require('webpack');
+const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: {
-    "things-scene-visualizer": ['./src/index.js']
+    'things-scene-visualizer': ['./src/index.js']
   },
   output: {
     path: path.resolve('./dist'),
-    filename: '[name].js',
+    filename: '[name].js'
   },
   resolve: {
     modules: ['./node_modules']
@@ -19,62 +19,77 @@ module.exports = {
     modules: ['./node_modules']
   },
   externals: {
-    "@hatiolab/things-scene": "scene"
+    '@hatiolab/things-scene': 'scene'
   },
   optimization: {
     minimize: true
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              'env', {
-                targets: {
-                  browsers: ['last 2 Chrome versions', 'Safari 10']
-                },
-                debug: true
-              }
-            ]
-          ],
-          plugins: []
-        }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  'env',
+                  {
+                    targets: {
+                      browsers: ['last 2 Chrome versions', 'Safari 10']
+                    },
+                    debug: true
+                  }
+                ]
+              ],
+              plugins: []
+            }
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        type: 'javascript/auto',
+        resourceQuery: /3d/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              emitFile: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|jpe?g|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              name: '[path][name].[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /obj[\w\/]+\.\w+$/,
+        exclude: /\.json$/,
+        resourceQuery: /3d/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              emitFile: false
+            }
+          }
+        ]
       }
-    }, {
-      test: /\.json$/,
-      type: 'javascript/auto',
-      resourceQuery: /3d/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          emitFile: false
-        }
-      }]
-    }, {
-      test: /\.(gif|jpe?g|png)$/,
-      loader: 'url-loader?limit=25000',
-      query: {
-        limit: 10000,
-        name: '[path][name].[hash:8].[ext]',
-        emitFile: false
-      }
-    }, {
-      test: /obj[\w\/]+\.\w+$/,
-      exclude: /\.json$/,
-      resourceQuery: /3d/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          emitFile: false
-        }
-      }]
-    }]
+    ]
   },
   plugins: [
     new UglifyJsPlugin({
