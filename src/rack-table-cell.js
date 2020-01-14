48117b3aac0037d938432c6f24bbe6db3decf7d0
lllleeee <lcj1123@hatiolab.com>
2020-01-14 15:33:18 +0900

    fix increase pattern was not worked
    #9

--- src/rack-table-cell.js
+++ src/rack-table-cell.js
@@ -3,55 +3,6 @@
  */
 import { Component, RectPath } from '@hatiolab/things-scene'
 
-const NATURE = {
-  mutable: false,
-  resizable: true,
-  rotatable: true,
-  properties: [
-    {
-      type: 'string',
-      label: 'section',
-      name: 'section'
-    },
-    {
-      type: 'string',
-      label: 'unit',
-      name: 'unit'
-    },
-    {
-      type: 'string',
-      label: 'shelf-locations',
-      name: 'shelfLocations',
-      placeholder: '1,2,3,... / ,,,04'
-    },
-    {
-      type: 'textarea',
-      label: 'bin-locations',
-      name: 'binLocations',
-      placeholder: '1,2,3,...'
-    },
-    {
-      type: 'checkbox',
-      label: 'is-empty',
-      name: 'isEmpty'
-    },
-    {
-      type: 'location-increase-pattern',
-      label: '',
-      name: ''
-    },
-    {
-      type: 'editor-table',
-      label: '',
-      name: '',
-      property: {
-        merge: false,
-        split: false
-      }
-    }
-  ]
-}
-
 const EMPTY_BORDER = {}
 
 function isBottomMost(idx, rows, columns) {
@@ -83,7 +34,63 @@ export default class RackTableCell extends RectPath(Component) {
   }
 
   get nature() {
-    return NATURE
+    return {
+      mutable: false,
+      resizable: true,
+      rotatable: true,
+      properties: [
+        {
+          type: 'string',
+          label: 'section',
+          name: 'section'
+        },
+        {
+          type: 'string',
+          label: 'unit',
+          name: 'unit'
+        },
+        {
+          type: 'string',
+          label: 'shelf-locations',
+          name: 'shelfLocations',
+          placeholder: '1,2,3,... / ,,,04'
+        },
+        {
+          type: 'textarea',
+          label: 'bin-locations',
+          name: 'binLocations',
+          placeholder: '1,2,3,...'
+        },
+        {
+          type: 'checkbox',
+          label: 'is-empty',
+          name: 'isEmpty'
+        },
+        {
+          type: 'location-increase-pattern',
+          label: '',
+          name: '',
+          property: {
+            event: {
+              'increase-location-pattern': event => {
+                var { increasingDirection, skipNumbering, startSection, startUnit } = event.detail
+                const rackTable = this.parent
+                rackTable.increaseLocation(increasingDirection, skipNumbering, startSection, startUnit)
+              }
+            }
+          }
+        },
+        {
+          type: 'editor-table',
+          label: '',
+          name: '',
+          property: {
+            merge: false,
+            split: false
+          }
+        }
+      ]
+    }
   }
 
   set merged(merged) {
@@ -113,6 +120,7 @@ export default class RackTableCell extends RectPath(Component) {
 
   get border() {
     var border = this.model.border || EMPTY_BORDER
+    return border
   }
 
   get isEmpty() {
